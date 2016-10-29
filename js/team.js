2b2f5a7e0356f9e66a4a35e1b468690a9ac37eaf
LeeJangHun <jjanghuni102@naver.com>
2016-10-30 04:30:50 +0900

    can load date

--- js/team.js
+++ js/team.js
@@ -8,6 +8,8 @@ $(function(){
 
 		drop:function(event, ui){
 			$(this).append($(ui.draggable.clone()));
+            console.log($(this).text());
+            addTL($(this).text());
             $(".article").append(addTimeLine);
 		}
 	});
@@ -18,7 +20,12 @@ $(function(){
 		helper: 'clone', // 복사 
 		//revertDuration:0, : 사용시 드레그 되돌아가는 에니메이션 없이 바로 귀환 
 		opacity:0.8, //클릭시 박스 투명도 지정
-        cursor:"pointer"
+        cursor:"pointer" //cursor pointer로 변경
 	});
+    
+    $(".monthBtn").click(function addMonth(){
+        
+    });
+    
     
 });
